c3dd6616528707e7b6ed317ba86c1c1a1080f88c
mohamed438201 <pes45056.9@gmail.com>
2025-09-30 22:09:43 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Statistics from "./components/Statistics";
 import HowItWorks from "./components/HowItWorks";
 import Footer from "./components/Footer";
 import Loader from "./components/Loader";
-import ContactPage from './components/ContactPage';
+import ContactPage from "./components/ContactPage";
+
+// مكون الصفحة الرئيسية
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Statistics />
+      <HowItWorks />
+      <Footer />
+    </>
+  );
+};
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -15,29 +29,20 @@ function App() {
     return () => clearTimeout(timer);
   }, []);
 
-  return (
-    <>
-      {loading ? (
-        <Loader />
-      ) : (
-        <div className="font-sans bg-gray-50 text-gray-900">
-          <Navbar />
-          <Hero />
-          <Statistics />
-          <HowItWorks />
-          <Footer />
-        </div>
-      )}
-    </>
-  );
+  if (loading) {
+    return <Loader />;
+  }
 
- return (
+  return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
+      <div className="font-sans bg-gray-50 text-gray-900 min-h-screen flex flex-col">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
     </Router>
   );
-  }
+}
+
 export default App;
