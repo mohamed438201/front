8d42dce0323848a695ff96485531b4d723970bc0
mohamed438201 <pes45056.9@gmail.com>
2025-09-30 21:20:21 +0300

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000', // <-- البورت والدومين المطلوب
+      target: 'mohamedsherif-sadq.syria-cloud.info/back/public', // <-- البورت والدومين المطلوب
       changeOrigin: true,
       timeout: 100000, // <-- زيادة المهلة إلى 60 ثانية لتجنب 504 Gateway Timeout
     })
